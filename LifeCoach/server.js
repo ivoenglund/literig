18c0f5674f22f0c2b880bbff277d173f2b2266fe
Ivo Englund <Ivo@macbook-pro.tailaafa5a.ts.net>
2026-07-30 03:12:46 +0200

    Enable recipe save requests and compact fields

--- LifeCoach/server.js
+++ LifeCoach/server.js
@@ -10,7 +10,7 @@ const pool = process.env.DATABASE_URL
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://literig.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
